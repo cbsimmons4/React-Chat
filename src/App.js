9e3bbac84b256d4584b292f249540f6036f89b6d
Cameron Simmons <cbsimmons4@gmail.com>
2020-04-27 01:59:35 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from './components/Layout';
+import './index.css';
+
+function App() {
+  return (
+    <Layout title ="chat App Baby"  />
+  );
+}
+
+export default App;
---
+++ src/Factories.js
@@ -0,0 +1,38 @@
+const uuidv4 = require ('uuid/v4'); 
+
+const createUser = ({name = ""} = {}) => { return (
+    {
+        id:uuidv4(),
+        name
+    }
+)}
+
+const createMessage = ({message = "", sender = ""} = {}) => (
+    {
+        id: uuidv4(),
+        time: getTime(new Date(Date.now())),
+        message,
+        sender
+    }
+)
+
+const createChat = ({messages = [], name = "Community", users = []} = {} ) => {
+    return {
+        id: uuidv4(),
+        name,
+        messages,
+        users,
+        typingUsers: []
+    }
+};
+
+
+const getTime = (date) => {
+    return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`
+}
+
+module.exports = {
+    createUser,
+    createMessage,
+    createChat
+}
